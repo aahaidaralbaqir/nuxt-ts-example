9438d20d665fb00c55b4c15d87957ba63cb19769
ahmadhdr <ahmadhdr.22@gmail.com>
2020-09-12 16:50:33 +0700

    test nuxt typescript todo app

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,10 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">
-        blog-nuxt
+        TODO APP
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <Logo />
     </div>
   </div>
 </template>
